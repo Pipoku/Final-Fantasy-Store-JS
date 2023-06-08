77dffd4c7142686fb7ab5d9e8e3158e707985026
Pipoku <39717526+Pipoku@users.noreply.github.com>
2023-06-08 16:53:52 -0300

    First Commit

---
+++ jquery.js
@@ -0,0 +1,21 @@
+
+let total
+let cantidad
+
+let calculoTotal = (precio,cantidad) =>{ total = total + precio * cantidad}
+
+console.log("Welcome Traveller!")
+console.log("Here you will find anything that you need, for the adventure!")
+console.log("You can see here the options, choose wisely but don't be afraid to do more than one!")
+let firstResponse = prompt("Do you wanna start? 1- 'Yes' 2- 'No' ")
+
+if(firstResponse == "Yes" || firstResponse == "yes"){
+    console.log("Let the fun begging!")
+    switch (firstResponse) {
+        case "Games":
+            console.log("ah shit here we go again")
+            break
+    
+    }
+    
+}
